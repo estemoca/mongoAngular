4c989ba091aff9bd76772e258d99cf83c4df9dca
esteban <estemoca@gmail.com>
2024-07-31 20:18:40 -0500

    nuevo proyecto

---
+++ backend/database.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+const URI = 'mongodb://localhost:27017/empleados';
+
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(db => console.log('DB is connected')).catch(err => console.error(err));
+module.exports = mongoose;
